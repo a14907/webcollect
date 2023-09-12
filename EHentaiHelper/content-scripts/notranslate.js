b7b04fe2beefc27695b4ca64b78a2c71bbc33a9f
wdq <a14907@outlook.com>
2023-09-12 11:00:06 +0800

    fix

--- EHentaiHelper/content-scripts/notranslate.js
+++ EHentaiHelper/content-scripts/notranslate.js
@@ -1,3 +1,10 @@
 
+if (window.location.host == 'www.newtonsoft.com') {
+    document.querySelectorAll('.codeSnippetContainerTabSingle').forEach(m => m.classList.add('notranslate'))
+    document.querySelectorAll('#tocNav').forEach(m => m.classList.add('notranslate'))
+    document.querySelectorAll('.tableSection').forEach(m => m.classList.add('notranslate'))
+}
 
-Object.values(document.querySelectorAll('.codeSnippetContainerTabSingle')).forEach(m=>m.classList.add('notranslate'))
+if (window.location.host == 'endjin.com') {
+    document.querySelectorAll('.js-table-wrap').forEach(m=>m.classList.add('notranslate'))
+}
