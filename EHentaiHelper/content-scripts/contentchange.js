a2904a6c58e0ed6c12061a749a0bf6c37876e504
wdq <a14907@outlook.com>
2023-09-13 14:49:13 +0800

    fix

--- EHentaiHelper/content-scripts/contentchange.js
+++ EHentaiHelper/content-scripts/contentchange.js
@@ -8,3 +8,12 @@ if (window.location.host == 'www.newtonsoft.com') {
 if (window.location.host == 'endjin.com') {
     document.querySelectorAll('.js-table-wrap').forEach(m=>m.classList.add('notranslate'))
 }
+
+if (window.location.host == 'tieba.baidu.com') {
+    document.querySelectorAll('.search-sec').forEach(m=>m.remove())
+    document.querySelectorAll('.top-sec').forEach(m=>m.remove())
+    document.querySelectorAll('.left-sec').forEach(m=>m.remove())
+    document.querySelectorAll('.userbar').forEach(m=>m.remove())
+    document.querySelectorAll('.r-top-sec').forEach(m=>m.remove())
+    document.querySelectorAll('.r-right-sec').forEach(m=>m.remove())
+}
