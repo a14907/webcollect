ff610ec3fdc78f90dd80746424f8408f48cfdb48
wdq <a14907@outlook.com>
2023-09-15 11:21:52 +0800

    fix

--- EHentaiHelper/content-scripts/contentchange.js
+++ EHentaiHelper/content-scripts/contentchange.js
@@ -13,6 +13,10 @@ if (window.location.host == 'devblogs.microsoft.com') {
     document.querySelectorAll('.table-responsive').forEach(m=>m.classList.add('notranslate'))
 }
 
+if (window.location.host == 'learn.microsoft.com') {
+    document.querySelectorAll('#left-container').forEach(m=>m.classList.add('notranslate'))
+}
+
 if (window.location.host == 'tieba.baidu.com') {
     setTimeout(function(){
         document.querySelectorAll('.userbar').forEach(m=>m.style.visibility='hidden')
